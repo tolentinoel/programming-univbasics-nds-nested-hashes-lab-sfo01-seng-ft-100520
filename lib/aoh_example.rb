969f9d1394c6613c36bb0fbc0408956e8dc56fe8
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-20 06:24:57 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,7 +20,8 @@ def literal_aoh
   {:name => "Joelle van Dyne", :occupation => "Radio Personality" }, 
   {:name => "Pat Monteseian", :occupation => "Staff"}, 
   {:name => "Kate Gompert", :occupation => "None"}, 
-  {:name => "Kate Gompert", :occupation => "None"} ]
+  {:name => "Bruce Green", :occupation => "Fan of Mildred"} 
+  ]
 end
 
 
